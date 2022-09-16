521100d9d2c3f3ed4ef75644cbbaa479c94b3b52
jhonnymestas <jhonnymestas@gmail.com>
2022-09-16 12:49:13 -0500

    correccion settings host prueba usuario

--- palmarpr/settings.py
+++ palmarpr/settings.py
@@ -95,11 +95,11 @@ WSGI_APPLICATION = 'palmarpr.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ['NAME'],
-        'USER': os.environ['USER'],
-        'PASSWORD': os.environ['PASSWORD'],
-        'HOST': os.environ['HOST'],
-        'PORT': os.environ['PORT'],
+        'NAME': 'palmar',
+        'USER': 'palmar',
+        'PASSWORD': 'palmar215577',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 # DATABASES = {
