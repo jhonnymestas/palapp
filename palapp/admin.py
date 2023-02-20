eccf6e751ee250a3d6388ec6d3195b711e49ac4c
jhonnymestas <jhonnymestas@gmail.com>
2023-02-20 10:46:13 -0500

    Busqueda admin

--- palapp/admin.py
+++ palapp/admin.py
@@ -51,7 +51,7 @@ class TipoDocAdmin(admin.ModelAdmin):
 
 class VentaAdmin(admin.ModelAdmin):
     list_display = ['id', 'cliente', 'y_o', 'doc_cotitular', 'nom_cotitular',  'terreno', 'vendedor', 'notaria', 'banco', 'condvta', 'nro_cont', 'fec_con',
-                    'preciod', 'precios', 'comision', 'observ', 'usuario_crea', 'foto_pago_com', 'fecha_pago_com',
+                    'plan_generado', 'preciod', 'precios', 'comision', 'observ', 'usuario_crea', 'foto_pago_com', 'fecha_pago_com',
                     'bco_pag_com', 'tdoc_sun_com', 'doc_pag_com', 'com_pag']
 
 
